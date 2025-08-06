30f64f31d9f447a98ff8bc2fc951e3ced6e173dd
Ritesh Narola <riteshnarola810@gmail.com>
2025-08-06 11:53:20 +0530

    New app deployed

---
+++ Forever app/src/pages/Collection.jsx
@@ -0,0 +1,24 @@
+import React, { useContext, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+
+function Collection() {
+
+  const { products } = useContext(ShopContext);
+  const [showFilter, setShowFilter] = useState(false);
+
+  return (
+    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
+      {/* Filter Options */}
+      <div className='min-w-60'>
+        <p className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS</p>
+        {/* Category Filter */}
+        <div className='border border-gray-300 pl-5 py-3 mt-6 ${}'>
+
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Collection
