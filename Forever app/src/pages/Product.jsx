bbd7e40e47e93a09f734f035574208e6539c2c83
Ritesh Narola <riteshnarola810@gmail.com>
2025-08-10 08:02:12 +0530

    Search bar Changes

--- Forever app/src/pages/Product.jsx
+++ Forever app/src/pages/Product.jsx
@@ -30,9 +30,9 @@ function Product() {
 
   return productData ? (
     <div className='border-t-2 pt-10  transition-opacity ease-in duration-500 opacity-100'>
-      {/* Product Data  */}
+      {/* ----------- Product Data ------------- */}
       <div className='flex gap-10 sm:gap-13 flex-col sm:flex-row'>
-        {/* Product Images  */}
+        {/* ----------- Product Images ------------- */}
          <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
             <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
                 {
