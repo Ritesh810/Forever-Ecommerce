4efda78aa6851e045ec169073637e4f50f56c2b2
Ritesh Narola <riteshnarola810@gmail.com>
2025-08-16 20:23:52 +0530

    About layout change

--- Forever app/src/pages/About.jsx
+++ Forever app/src/pages/About.jsx
@@ -8,7 +8,7 @@ function About() {
       <div className='text-2xl text-center pt-8 border-t'> 
           <Title text1={'ABOUT'} text2={'US'} />
       </div>
-      <div className='my-10 flex flex-col md:flex-col md:flex-row gap-16'>
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
         <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="about img" />
         <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
           <p>Forever was born out of a passion for innovation and a desire to revolutionize the way people shop online. Our journey began with a simple idea: to provide a platform where customers can easily discover, explore, and purchase a wide range of products from the comfort of their homes.</p>
