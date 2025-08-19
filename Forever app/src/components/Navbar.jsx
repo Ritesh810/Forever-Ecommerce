7f423761624f578839c1b879efdef2b1c6ead79d
Ritesh Narola <riteshnarola810@gmail.com>
2025-08-19 16:40:09 +0530

    Nav bar reviewed and optimized

--- Forever app/src/components/Navbar.jsx
+++ Forever app/src/components/Navbar.jsx
@@ -15,7 +15,6 @@ function Navbar() {
         <img src={assets.logo} className="w-36" alt="Forever" />
       </Link>
       
-
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700 ">
         <NavLink to="/" className="flex flex-col items-center gap-1">
           <p>HOME</p>
