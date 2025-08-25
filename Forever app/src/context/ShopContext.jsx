5eb2e36fd49e07b5ab3cf8c4e7ee44439d7225ad
Ritesh Narola <riteshnarola810@gmail.com>
2025-08-25 19:21:22 +0530

    Shop Context Changes

--- Forever app/src/context/ShopContext.jsx
+++ Forever app/src/context/ShopContext.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
-
+    
     const currency = '$';
     const delivery_fee = 10;
     const [search, setSearch] = useState('')
